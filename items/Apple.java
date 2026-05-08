5cd3f0619addbc0d030f9b762927f1d36b7fd579
Said Aghamaliyev <digitalwarior666@gmail.com>
2026-05-08 20:43:43 +0400

    Create Apple.java

---
+++ items/Apple.java
@@ -0,0 +1,13 @@
+package items;
+
+public class Apple extends Food {
+  
+  public Apple(){
+    super("Apple", 5);
+  }
+
+  @Override
+  public void use(){
+    // Sound/Animation of eating
+  }
+}
